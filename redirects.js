001001833cda65e6b660694cb4a80a4d52154774
Noctrow <Noctrow.Oficial@gmail.com>
2024-12-18 02:06:11 -0300

    update redirects.js

--- redirects.js
+++ redirects.js
@@ -1,5 +1,6 @@
 // Variable existente con algunos enlaces
 var redirects = {
   "FB": "https://www.facebook.com/chochoxapp",
+  "toonx": "https://toonx.net",
   "c080s9": "https://blog.chochoxapp.com/2024/12/actualizacion.html"
 };
